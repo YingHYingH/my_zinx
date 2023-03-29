7b4e5e6cbe5aae2d0259011fc1bde5cb95f763aa
wangyinghao.0609 <wangyinghao.0609@bytedance.com>
2023-03-29 10:51:52 +0800

    v0.8
    Change-Id: I958fd3c5247806d5432b04a859b45bb4edccd398

--- ziface/imsgHandler.go
+++ ziface/imsgHandler.go
@@ -9,4 +9,8 @@ type IMsgHandler interface {
 	DoMsgHandler(request IRequest)
 	// 为消息添加具体的处理逻辑
 	AddRouter(msgID uint32, router IRouter)
+
+	StartWorkerPool()
+
+	SendMsgToTaskQueue(request IRequest)
 }
--- znet/msgHandler.go
+++ znet/msgHandler.go
@@ -2,16 +2,25 @@ package znet
 
 import (
 	"fmt"
+	"my_zinx/utils"
 	"my_zinx/ziface"
 )
 
 type MsgHandler struct {
 	// 存放每个MsgID对应的处理方法
 	Apis map[uint32]ziface.IRouter
+	// 负责Worker取任务的消息队列
+	TaskQueue []chan ziface.IRequest
+	// 业务工作Worker池的worker数量
+	WorkerPoolSize uint32
 }
 
 func NewMsgHandler() *MsgHandler {
-	return &MsgHandler{Apis: map[uint32]ziface.IRouter{}}
+	return &MsgHandler{
+		Apis:           map[uint32]ziface.IRouter{},
+		WorkerPoolSize: utils.GlobalObject.WorkerPoolSize,
+		TaskQueue:      make([]chan ziface.IRequest, utils.GlobalObject.WorkerPoolSize),
+	}
 }
 
 func (m *MsgHandler) DoMsgHandler(request ziface.IRequest) {
@@ -31,4 +40,35 @@ func (m *MsgHandler) AddRouter(msgID uint32, router ziface.IRouter) {
 		return
 	}
 	m.Apis[msgID] = router
+}
+
+// 启动一个Worker工作池（开启工作池的动作只能发生一次，一个zinx框架只能有一个Worker工作池）
+func (m *MsgHandler) StartWorkerPool() {
+	// 根据workerPoolSize分别开启worker
+	for i := 0; i < int(m.WorkerPoolSize); i++ {
+		// 当前的worker对应的channel消息队列开辟空间
+		m.TaskQueue[i] = make(chan ziface.IRequest, utils.GlobalObject.MaxWorkerTaskLen)
+		go m.StartOneWorker(i, m.TaskQueue[i])
+	}
+}
+
+// 启动一个Worker工作流程
+func (m *MsgHandler) StartOneWorker(workerID int, taskQueue chan ziface.IRequest) {
+	fmt.Println("workerID = ", workerID, " is started...")
+
+	// 阻塞等待消息队列的消息
+	for {
+		select {
+		case request := <-taskQueue:
+			m.DoMsgHandler(request)
+		}
+	}
+}
+
+// 将消息交给TaskQueue，由worker处理
+func (m *MsgHandler) SendMsgToTaskQueue(request ziface.IRequest) {
+	// 将消息平均分配给不同worker
+	workID := request.GetConnection().GetConnID() % m.WorkerPoolSize
+	// 将消息发送给对应worker的taskQueue
+	m.TaskQueue[workID] <- request
 }
